c31ffd59af170c339aaac631348c98d53b7e641b
DonkeyCo <duc.vongoc01@gmail.com>
2021-05-15 00:18:52 +0200

    Reset UI Modifications/Changes

--- src/main/java/dev/donkz/pendragon/App.java
+++ src/main/java/dev/donkz/pendragon/App.java
@@ -1,15 +1,45 @@
 package dev.donkz.pendragon;
 
-import dev.donkz.pendragon.domain.player.Player;
-import dev.donkz.pendragon.exception.infrastructure.EntityNotFoundException;
-import dev.donkz.pendragon.infrastructure.persistence.local.LocalPlayerRepository;
-import dev.donkz.pendragon.ui.MainWindow;
-import javafx.application.Application;
-import javafx.stage.Stage;
+import dev.donkz.pendragon.exception.infrastructure.IndexAlreadyExistsException;
+import dev.donkz.pendragon.exception.infrastructure.MultiplePlayersException;
+import dev.donkz.pendragon.exception.model.RequiredAttributeMissingException;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws RequiredAttributeMissingException, IndexAlreadyExistsException, MultiplePlayersException {
         System.out.println("Hello World");
-        Application.launch(MainWindow.class, args);
+//
+//        CampaignRepository repo = new LocalCampaignRepository();
+//        PlayerRepository pRepo = new LocalPlayerRepository();
+//
+//        Pc pc = new Pc();
+//        pc.setId("ID");
+//        pc.setCharacterInformation(new CharacterInformation("Chara", "", "", "", "Kind", "Race"));
+//
+//        Player pl = Player.builder().username("Donkey").character(pc).build();
+//        pRepo.save(pl);
+//
+//        Player p = null;
+//        try {
+//            p = pRepo.findRegisteredPlayer();
+//        } catch (MultiplePlayersException e) {
+//            e.printStackTrace();
+//        }
+//
+//        CampaignVariant variant = new CampaignVariant();
+//        variant.setCreator(p);
+//        variant.setId("ABCDDE");
+//        variant.setName("DnD 4e");
+//
+//        Campaign c = null;
+//        try {
+//            c = Campaign.builder().name("Campaign B").dm(pl).campaignVariant(variant).character(pc).player(pl).build();
+//        } catch (RequiredAttributeMissingException e) {
+//            e.printStackTrace();
+//        }
+//        try {
+//            repo.save(c);
+//        } catch (IndexAlreadyExistsException e) {
+//            e.printStackTrace();
+//        }
     }
 }
---
+++ src/main/java/dev/donkz/pendragon/service/CharacterListingService.java
@@ -0,0 +1,25 @@
+package dev.donkz.pendragon.service;
+
+import dev.donkz.pendragon.domain.campaign.CampaignRepository;
+import dev.donkz.pendragon.domain.character.Pc;
+import dev.donkz.pendragon.domain.character.PcRepository;
+import dev.donkz.pendragon.domain.player.Player;
+import dev.donkz.pendragon.domain.player.PlayerRepository;
+import dev.donkz.pendragon.exception.infrastructure.MultiplePlayersException;
+
+import java.util.List;
+
+public class CharacterListingService {
+    private final PcRepository pcRepository;
+    private final PlayerRepository playerRepository;
+
+    public CharacterListingService(PcRepository pcRepository, PlayerRepository playerRepository) {
+        this.pcRepository = pcRepository;
+        this.playerRepository = playerRepository;
+    }
+
+    public List<Pc> getPlayerCharacters() throws MultiplePlayersException {
+        Player player = playerRepository.findRegisteredPlayer();
+        return player.getCharacters();
+    }
+}
